6052eb5b7d553640ffb6b67270149d2e4b93fdfa
Viktor <shiryakovv@gmail.com>
2019-04-02 14:08:47 +0700

    Доработки

--- src/views/item/index.php
+++ src/views/item/index.php
@@ -33,16 +33,26 @@ $this->title = ($relation ? $relation->getItem() . ' / ' : '') . ' ' . Yii::t('i
     <div class="box-body">
 
         <?=\svsoft\yii\items\widgets\ItemGridView::widget(\yii\helpers\ArrayHelper::merge([
+            'id' =>'item-grid',
+            'class'=>\svsoft\yii\items\widgets\ItemGridView::class,
             'dataProvider' => $dataProvider,
             'itemType' => $itemType,
             'labels' => $labels->fields,
+            'rowOptions' => function ($model, $key) use ($relation) {
+                                return [
+                                        'data'=>[
+                                                'href'=> \yii\helpers\Url::to(['item/update', 'id' => $key, 'relation' => (string)$relation]),
+                                        ]
+                                ];
+                            }
+            ,
             'columns' => [
                 [
                     'class' => \yii\grid\ActionColumn::class,
                     'contentOptions' => ['class' => 'text-center'],
                     'options' => ['class'=>'options'],
                     'visibleButtons' => [
-                            'view'=>false,
+                        'view' => false,
                     ],
                     'buttonOptions' => ['class'=>['btn btn-default btn-sm']],
                     'urlCreator' => function ($action, $model, $key) use ($relation) {
@@ -53,3 +63,13 @@ $this->title = ($relation ? $relation->getItem() . ' / ' : '') . ' ' . Yii::t('i
         ], $config));?>
     </div>
 </div>
+
+<?php
+$js = <<< JS
+$('#item-grid > table > tbody tr').dblclick(function() {
+    window.location = $(this).data('href');
+    
+})
+JS;
+$this->registerJs($js);
+?>
